9699e9a7440dc80bf762065cbdb0a4eec9b5836e
sigis1980 <sigis1980@gmail.com>
2014-02-03 15:16:15 +0200

    pataisymas

--- Antras/3.rb
+++ Antras/3.rb
@@ -4,4 +4,4 @@ var1=gets.chomp
 puts 'Kokia tavo pavarde?'
 var2=gets.chomp
 var3=var1.length + var2.length
-puts 'Tavo varde ir pavardeje yra - ' + (var3).to_s + ' ?'
+puts 'Tavo varde ir pavardeje yra - ' + (var3).to_s + ' raidziu?'
