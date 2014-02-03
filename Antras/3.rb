bbb0211efa1a1dec40fb15e388e089e67b95ef9d
sigis1980 <sigis1980@gmail.com>
2014-02-03 15:14:05 +0200

    Raidziu skaiciuokle

--- Antras/3.rb
+++ Antras/3.rb
@@ -1,1 +1,7 @@
-#quiz
+#suskaiciuoja kiek varde ir pavardeje yra raidziu, bet ignoruoja tarpus
+puts 'Koks tavo vardas?'
+var1=gets.chomp
+puts 'Kokia tavo pavarde?'
+var2=gets.chomp
+var3=var1.length + var2.length
+puts 'Tavo varde ir pavardeje yra - ' + (var3).to_s + ' ?'
