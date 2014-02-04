c0065ce88f2b061444c79a33e45ff6d84646eabf
sigis1980 <sigis1980@gmail.com>
2014-02-04 21:00:17 +0200

    4 naujas

--- Antras/4.rb
+++ Antras/4.rb
@@ -1,2 +1,9 @@
-puts 'ant'  
-puts
+#pijoku daina
+skaicius_99 = 99
+while skaicius_99 != 0
+puts '" '+skaicius_99.to_s+' Bottles of Beer on the Wall"'
+skaicius_99 = skaicius_99-1
+
+puts 'Take one and pass it around, ' + skaicius_99.to_s + ' of beer on the wall.'
+end
+puts 'No more beer, and we\'re not drunk yet.'
--- pirmas.rb
+++ pirmas.rb
@@ -1,3 +1,4 @@
+*treniruotes nevertos perziuros Arturai
 vardas = 'Ponas Jona Andrius Grybauskas Pauliukaitis'
 puts 'mano vardas yra ' + vardas + '.'
 puts 'Wow! ' + vardas
@@ -49,3 +50,71 @@ puts(rand(49))
 puts(rand(49))
 
 puts srand(0)/10000
+
+puts 'ant' < 'Zoo'
+puts 'ant'.downcase < 'Zoo'.downcase
+
+puts 'hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Rastida'
+  puts 'What a lovely name!'
+end
+
+puts 'Labas, ash Orakulas galiu nuspeti tavo ateiti. Pasakyk savo varda?'
+name = gets.chomp
+puts 'Labas, ' + name + '.'
+if name == 'Rastida'
+  puts '*: !saskes sukiup ekual sevaT ineidatkneP'.reverse
+elsif name == 'Sigitas'
+  puts ': sevaT ri sak stap sat ekual oJ)'.reverse
+else
+  puts'!!!sunorakam inibak'.reverse
+end
+
+
+
+var1 =''
+while var1 != 'bye'
+  puts var1
+  var1 = gets.chomp
+
+end
+puts 'come again'
+
+puts 'Koks tavo vardas?'
+var1=gets.chomp
+puts 'Labas '+var1
+if var1=='Sigitas'
+  puts 'Pavarde Dilys?'
+elsif var1=='Rastida'
+  puts 'Pavarde Diliene?'
+end
+
+
+puts 'Koks tavo vardas?'
+var1=gets.chomp
+puts 'Labas '+var1
+if var1=='Sigitas'||var1=='Rastida'
+  puts 'Koks grazus vardas?'
+
+end
+request = ''
+while request != 'stop'
+  puts 'What would you like to ask C to do?'
+  request = gets.chomp
+  puts 'You say, "C, please" ' + request +' ?"'
+  puts
+  puts 'C\'s response :'
+  puts '"Papa '+ request+'."'
+  puts '"Mama '+ request+'."'
+  puts '"Ruby '+ request+'."'
+  puts '"Nono '+ request+'."'
+  puts '"Emma '+ request+'."'
+  puts
+
+
+end
+puts 'Spyga taukuota'
+O_a = gets.chomp
+puts 'Nesamone '+O_a
