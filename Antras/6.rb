521abca9a5f998bc77ace8e06dc2a2974f858f3b
sigis1980 <sigis1980@gmail.com>
2014-02-07 21:57:48 +0200

    bugas

--- Antras/6.rb
+++ Antras/6.rb
@@ -13,13 +13,13 @@ while mt_2.to_i >= mt_1.to_i
   mt_3=mt_2%4
   mt_4=mt_2%100
   mt_5=mt_2%400
-    if mt_3 == 0 && mt_4%100 != 0
+  if mt_3 == 0 && mt_4%100 != 0
     puts mt_2
   elsif
     mt_5 == 0
     puts mt_2
     else
-
     end
   mt_2=mt_2-4
 end
+puts 1985 >= 2014
